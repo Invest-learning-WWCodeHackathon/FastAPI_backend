634341a94696d914d8243fee25c3aa21cd1db0bf
Sharmila Thippabhotla <sharmilatd2@gmail.com>
2023-10-23 03:21:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,34 +7,43 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.encoders import jsonable_encoder
 from starlette.responses import RedirectResponse
 
-from langchain.chains import LLMChain, ConversationChain
-from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, ConversationChain, ConversationalRetrievalChain
+from langchain.prompts import PromptTemplate, ChatPromptTemplate
 from langchain.llms import OpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chat_models import ChatOpenAI
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
-from langchain.vectorstores import Chroma
+from langchain.vectorstores import Chroma, DocArrayInMemorySearch
 from langchain.output_parsers import PydanticOutputParser
 from langchain.pydantic_v1 import BaseModel, Field, validator
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
 from supabase import create_client, Client
 
-from apikey import apikey
+from apikey import apikey, supabasekey
 from search_all_stock_tickers import Stock, StockSearchEngine
 import yfinance as yf
 from concurrent.futures import ThreadPoolExecutor
 import openai
 
-#Include openai api key and supabase api key
+os.environ['OPENAI_API_KEY'] = apikey
+
+API_URL = 'https://otelrzfgawehviamvzox.supabase.co'
+API_KEY = supabasekey
 
 supabase: Client = create_client(API_URL, API_KEY)
 
-loader = TextLoader('learn_docs/lesson1.txt')
-docs = loader.load()
+loader1 = TextLoader('learn_docs/level1.txt')
+#docs1 = loader.load()
+index1 = VectorstoreIndexCreator().from_loaders([loader1])
 
-index = VectorstoreIndexCreator().from_loaders([loader])
+loader2 = TextLoader('learn_docs/level2.txt')
+index2 = VectorstoreIndexCreator().from_loaders([loader2])
+
+loader3 = TextLoader('learn_docs/level3.txt')
+index3 = VectorstoreIndexCreator().from_loaders([loader3])
 
 app = FastAPI(title = "Youth Investment Learning App")
 
@@ -62,7 +71,12 @@ def main():
 ################################################################
 
 @app.get("/quizQuestion")
-def get_quiz_question():
+def get_quiz_question(user_id: str):
+
+  data, count = supabase.table('userData').select('*').eq("user_id", user_id).execute()
+
+  level = data[1][0]["level"]
+    
   parser = PydanticOutputParser(pydantic_object = QuizQuestion)
   prompt = PromptTemplate(
     template = "Answer the user query.\n{format_instructions}\n{query}",
@@ -70,51 +84,56 @@ def get_quiz_question():
     partial_variables={"format_instructions":parser.get_format_instructions()},
   )
 
-  _input = prompt.format_prompt(query = "Generate a quiz question using the context in the text given here.")
-  response = index.query(_input.to_string(), llm = ChatOpenAI(temperature = 0.9, model = "gpt-3.5-turbo-0613"))
+  _input = prompt.format_prompt(query = "Generate a question using the context in the text given here.")
   
+  if level == 1:
+    response = index1.query(_input.to_string(), llm = ChatOpenAI(temperature = 0.9, model = "gpt-3.5-turbo-0613"))
+  elif level == 2:
+    response = index2.query(_input.to_string(), llm = ChatOpenAI(temperature = 0.9, model = "gpt-3.5-turbo-0613"))
+  elif level == 3:
+    response = index3.query(_input.to_string(), llm = ChatOpenAI(temperature = 0.9, model = "gpt-3.5-turbo-0613"))
+
   return parser.parse(response)
 
 ################################################################
 
-#@app.post("/learnBot")
-async def learning_content(userId: str):
+@app.post("/learnBot")
+async def learning_content(userId: str, userInput: str):
   # Get user's level from db
-  user_level = supabase.table("userData").select("*").eq("id", userId).execute()
+  data, count = supabase.table("userData").select("*").eq("user_id", userId).execute()
 
-  topics = ["money", "saving", "stocks"]
-  #if user_level >= 0 and user_level <= 5:
-    #Generate easy learning content from level 1 topics
-  #elif user_level >= 5 and user_level <=20:
-    #Generate content from level 2 topics
-  #elif user_level >= 20 and user_level <= 50:
-    #Generate final level content
+  user_level = data[1][0]["level"]
 
+  loader = TextLoader('learn_docs/level1.txt')
+  documents = loader.load()
 
-  #Implement chatbot conversational capability
+  text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 150)
+  docs = text_splitter.split_documents(documents)
 
-  prompt1 = PromptTemplate(
-    template = "Display the following {topics} as user options pulling from the current context",
-    input_variables=["topics"],
-  )
-
-  prompt2 = PromptTemplate(
-    template = "Generate educational content for a teen user on {topic}",
-    input_variables=["topic"],
-  )
-
-  llm = ChatOpenAI(temperature = 0.5, model = "gpt-3.5-turbo-0613")
+  embeddings = OpenAIEmbeddings()
+  db = DocArrayInMemorySearch.from_documents(docs, embeddings)
+  retriever = db.as_retriever(search_type = "similarity", search_kwargs={"k":4})
   
-  #conversation = ConversationChain(
-   # prompt = prompt1,
-    #llm = llm,
-    #verbose = True,
-    #memory = ConversationBufferMemory(ai_prefix = "LearnBot")
-    #)
-  
-  #response = conversation.predict(topics = topics) 
-  #return response
+  # Create chatbot for level 1 users
 
+  if user_level == 1:
+
+    chat_template = ChatPromptTemplate.from_messages([
+      ("system", "You are a teaching assistant providing comprehensive lessons for teenagers. Provide lessons geared towards 17 year olds in simple language and abundant examples. First introduce yourself as LearnBot and then give the user a list of topics from:\n1. What Is a Stock? An Introduction for Teens\n2.Why Do Companies Issue Stocks?\n3.How to Buy Your First Stock as a Teenager\n4.The Role of Stock Exchanges: NYSE, NASDAQ, and Others\n5.What Happens at a Stock Market Opening and Closing?.\n Based on the user's input, generate the next message. Ask the user questions to make them think and learn the concepts in greater detail. Use the sample conversation below to guide you:\n LearnBot: Hello! I'm LearnBot, your helpful teaching assistant. Choose a topic you'd like to learn about today. 1. What is a stock? 2. Why do companies issue stocks? 3. What is the role of the stock exchange?\nUser: Tell me more about what stocks are \n LearnBot: Sure. Let us begin with an analogy *end of example* and continue the conversation in the same manner"),
+      ("human", "What is the topic of the lesson?"),
+      ("ai", "What are stocks"),
+      ("human", "{user_input}"),
+    ])
+    
+    llm = ChatOpenAI(temperature = 0.9, model = "gpt-3.5-turbo-0613")
+    memory = ConversationBufferMemory(memory_key = "chat_history", return_messages = True)
+    
+    conversation = LLMChain(llm = llm, prompt = chat_template, verbose = True, memory = memory)
+    response = conversation({"user_input": userInput})    
+    
+    return response
+
+  
 ################################################################
 
 @app.get("/stock/info")
@@ -125,15 +144,23 @@ def get_stock_info(ticker: str):
 
 ################################################################
 
+@app.get("/stock/price")
+def get_stock_price(ticker: str):
+  stock = yf.Ticker(ticker)
+  stock_price = stock.info["currentPrice"]
+  return jsonable_encoder(stock_price)
+  
+################################################################
+
 @app.get("/stock/list_all_tickers")
-def list_all_tickers():
+def list_all_tickers(pages: int, per_page: int):
   engine = StockSearchEngine()
-  tickers = engine.list_all_tickers(page = 1, per_page = 5)
+  tickers = engine.list_all_tickers(page = pages, per_page = per_page)
   return tickers
 
 ################################################################
 
-@app.get("/stock/multiple_info")
+#@app.get("/stock/multiple_info")
 def get_multiple_stock_info(tickers):
 
   #Check implementation with Krystal
@@ -147,23 +174,40 @@ def get_multiple_stock_info(tickers):
 
 #Implement access to supabase db CRUD operations
 
-#@app.post("/db/insert")
-def create_new_user():
-  data, count = supabase.table('userData').insert({"id": 20001, "age": 15, "level" : 1, "points" : 1000}).execute()
+@app.post("/db/insert")
+def create_new_user(user_id: str):
 
-#@app.get("/db/get_by_id")
-def get_row_by_userid():
-  response = supabase.table('userData').select('*').eq('id', 20001).execute()
-  return response
+  uData, uCount = supabase.table('userData').select("*", count = 'exact').eq("user_id", user_id).execute() 
+
+  if uCount[1] == 0 or uCount[1] == None:
+    #User is new
+    data, count = supabase.table('userData').insert({"user_id": user_id, "level" : 1, "points" : 1000}).execute()
+    return jsonable_encoder({"New User": True})
+  else:
+    return jsonable_encoder({"New User": False})
+    
+################################################################
+
+@app.get("/db/get_by_id")
+def get_row_by_userid(user_id: str):
+  response = supabase.table('userData').select('*').eq('user_id', user_id).execute()
+  return jsonable_encoder(response)
+
+################################################################
 
 #@app.get("/db/get_all")
 def get_all_rows():
   response = supabase.table('userData').select('*').execute()
   return response
 
+################################################################
+
 #@app.post("/db/update")
 def update_row():
   data, count = supabase.table('userData').update({"age": 16}).eq('id', 20001).execute()
+
+
+
 
 #@app.post("/db/delete")
 def delete_row():
@@ -171,9 +215,10 @@ def delete_row():
 
 ################################################################
 
-@app.post("/stock/trend")
+@app.get("/stock/trend")
 def get_stock_trend(ticker: str):
   stock = yf.Ticker(ticker)
+  stock_info = stock.info
   stock_history = stock.history(period = "1y", interval = "1mo")
   stock_history = stock_history.drop(columns = ["Open"])
   stock_history = stock_history.drop(columns = ["Close"])
@@ -185,6 +230,8 @@ def get_stock_trend(ticker: str):
       encoded_stock_history = stock_history.reset_index().to_dict('records')
       for data_point in encoded_stock_history:
         data_point["Average"] = (data_point["High"] + data_point["Low"]) / 2
+
+      encoded_stock_history.append({"Name": stock_info["longName"], "CurrentPrice" : stock_info["currentPrice"]})
     
   except Exception as e:
       raise HTTPException(status_code=500, detail=f"Error encoding stock history data: {e}")
@@ -204,22 +251,82 @@ def get_stock_trend(ticker: str):
 
 ################################################################
 
-#@app.post("/stock/buy_new")
-async def buy_new_stocks(ticker: str, quantity: int, price: int, user_id : str):
+@app.get("/stock/buy_stocks")
+async def buy_stocks(ticker: str, quantity: float, buying_price: float, current_points: float, user_id : str):
+
+  userdata, usercount = supabase.table('userData').update({"points" : current_points}).eq('user_id', user_id).execute()
+  stockdata, stockcount = supabase.table('stockData').select('*', count = 'exact').eq('user_id', user_id).eq('ticker', ticker).execute()
+
+  #print(stockdata)
+  #print(stockcount)
   
-  stock = Stock(ticker)
-  stock_info = stock.get_info()
-  stock_price = stock_info["currentPrice"]
-  total_cost = stock_price * quantity
+  if (stockcount[1] == None) or (stockcount[1] == 0):
+    print("new stock for this user")
+    data, count = supabase.table('stockData').insert({"user_id": user_id, "ticker": ticker, "quantity": quantity, "equity": (quantity * buying_price)}).execute()
+    return True
+  
+  elif stockcount[1] > 0:
+    #Stocks from this company have been bought.. update values instead
+    print("stock already exists, updating counts")
+    current_quantity = stockdata[1][0]['quantity']
+    updated_quantity = current_quantity + quantity
+    equity_gained = buying_price*quantity
+    updated_equity = stockdata[1][0]['equity'] + equity_gained
+    data, count = supabase.table('stockData').update({"quantity" : updated_quantity, "equity": updated_equity, "buying_price": buying_price}).eq('user_id', user_id).eq('ticker', ticker).execute()
+    return True
 
-  #Assumes that front end verifies there are enough points to buy
-  #Update the db with new stock info and update point balance
-  data,count = supabase.table('userData').select("*").eq('id', user_id).execute()
-
-  point_balance = data[1][0]['points'] - total_cost
-  data, count = supabase.table('userData').update({"points": point_balance}).eq('id', user_id).execute()
-  data1, count1 = supabase.table('userData').update({"stocks": {"ticker": ticker, "quantity": quantity, "equity": total_cost}}).eq('id', user_id).execute()
-
-  return data
+  return False
 
 ###################################################################
+
+@app.get("/stock/sell_stocks")
+async def sell_stocks(ticker: str, quantity: float, selling_price: float, current_points: int, user_id: str):
+  userdata, usercount = supabase.table('userData').update({"points" : current_points}).eq('user_id', user_id).execute()
+  stockdata, stockcount = supabase.table('stockData').select('*', count = 'exact').eq('user_id', user_id).eq('ticker', ticker).execute()
+  
+  if (stockcount[1] == None) or (stockcount[1] == 0):
+    print("no stocks to sell")
+    return False
+  
+  elif stockcount[1] > 0:
+    #Stocks from this company have been sold.. update values instead
+    print("stock already exists, updating counts")
+    current_quantity = stockdata[1][0]['quantity']
+    updated_quantity = current_quantity - quantity
+    equity_lost = selling_price*quantity
+    updated_equity = stockdata[1][0]['equity'] - equity_lost
+    data, count = supabase.table('stockData').update({"quantity" : updated_quantity, "equity": updated_equity}).eq('user_id', user_id).eq('ticker', ticker).execute()
+    return True
+    
+  return False
+
+###################################################################
+
+@app.get("/stock/get_user_stock_portfolio")
+async def get_user_stock_portfolio(user_id: str):
+
+  data, count = supabase.table('stockData').select('*').eq('user_id', user_id).execute()
+  return jsonable_encoder(data)
+
+###################################################################
+
+@app.get("/update_points")
+async def update_points(user_id: str, current_points: int, was_correct: bool):
+
+  data, count = supabase.table('userData').select('*').eq('user_id', user_id).execute()
+  
+  answers = int(data[1][0].get("correct_answers", 0))
+  
+  if(was_correct):
+    answers = answers+1
+
+  if answers > 10:
+    new_level = data[1][0]["level"] + 1
+  else:
+    new_level = data[1][0]["level"]
+  
+  userdata, usercount = supabase.table('userData').update({"level": new_level, "points" : current_points, "correct_answers" : answers}).eq('user_id', user_id).execute()
+  return ("User points updated")
+
+###################################################################
+
